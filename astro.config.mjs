e21696cbf4108bc9145aa6e23a365914a4783e5f
Nelson-5553 <113390410+Nelson-5553@users.noreply.github.com>
2025-06-25 16:16:37 -0500

    Fix WebSocket connection for Astro dev server

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 
     vite: {
+        server: {
+            host: true
+        },
         resolve: {
             alias: {
                 '@docs': '/content/docs',
